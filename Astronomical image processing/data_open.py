b539bef79d676f2595962b9e8cda6e77a036d4fa
tollbran <brandon.tollan@me.com>
2021-01-14 13:36:49 +0000

    Added cropping functionality
    Crops sides of image.

--- Astronomical image processing/data_open.py
+++ Astronomical image processing/data_open.py
@@ -13,10 +13,14 @@ import scipy as sp
 
 class DataGeneration:
     
-    def __init__(self, hdulist=fits.open("data/A1_mosaic.fits")):
+    def __init__(self, hdulist=fits.open("data/A1_cropped_mosaic.fits")):
         self.hdulist = hdulist
         self.astro_flux = hdulist[0].data
         self.raw_data = []
+        self.y_bottom_border = 119
+        self.y_top_border =  4518
+        self.x_left_border = 98
+        self.x_right_border = 2476
         
     def get_raw_data(self):
         
@@ -40,6 +44,37 @@ class DataGeneration:
             y_counter +=1
 
         self.raw_data = new_data
+        
+    def crop_edges(self, old_data):
+        
+        '''
+        This function crops the edges of the data. ie. it removes all the
+        data between the ranges specified by the user. This shouldn't
+        need to be used again as I have saved the cropped data as a
+        fits file so can be used freely
+        
+        '''
+        
+        y_counter = len(old_data) - 1
+        x_counter = len(old_data[0]) - 1
+        sp.asarray(old_data)
+        
+        while y_counter != -1:
+            if not (self.y_bottom_border <= y_counter <= self.y_top_border):
+                #removes the whole rows froma 2D array
+                old_data = sp.delete(old_data,y_counter,0)
+            y_counter -=1
+        
+        while x_counter != -1:
+            if not (self.x_left_border <= x_counter <= self.x_right_border):
+                #removes the whole columns from a 2d array
+                old_data = sp.delete(old_data,x_counter,1)
+            x_counter -=1
+                
+        self.astro_flux = old_data
+        
+    def remove_blooming(self):
+        
         
     
 
--- Astronomical image processing/main.py
+++ Astronomical image processing/main.py
@@ -7,7 +7,9 @@ Created on Wed Jan 13 13:41:31 2021
 import data_open as dataf
 import histogram_gen as histo
 
+test = [[1,2,89,3,4],[1,2,3,4,5],[1,3,4,35,5],[4,5,6,7,5]]
 dataFrame = dataf.DataGeneration()
-dataFrame.convert_2d_1d(dataFrame.astro_flux)
+#dataFrame.convert_2d_1d(dataFrame.astro_flux)
 
-histo.gen_histogram(dataFrame.get_raw_data())
+print(dataFrame.astro_flux)
+#histo.gen_histogram(dataFrame.get_raw_data())
